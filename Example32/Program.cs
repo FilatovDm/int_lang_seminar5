a1e229c2ff88a2eef7564275248508b7104d9fdd
Dmitriy Filatov <filatov-d@mail.ru>
2022-09-16 12:00:05 +0300

    Example 32 completed

---
+++ Example32/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 32: Напишите программу замена элементов массива: положительные
+элементы замените на соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, 8, 2]
+*/
+
+int[] GetArrayReplacement(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * -1;
+    }
+
+    return array;
+}
+
+int[] array = {-4, -8, 8, 2};
+var arrayReplacment = GetArrayReplacement(array);
+
+Console.WriteLine("[{0}]", string.Join(", ", arrayReplacment));
